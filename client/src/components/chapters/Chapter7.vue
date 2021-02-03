fd22bb3959a9c2c16057ff876aab70ff9d7b4ec3
Sandravaphilips <lacasandra1@gmail.com>
2021-02-03 04:17:58 -0800

    Built Chapter7

---
+++ client/src/components/chapters/Chapter7.vue
@@ -0,0 +1,212 @@
+<template>
+    <section class="akn-chapter" id="chp_7" data-eid="chp_7">
+        <h2>
+            Chapter 7
+            <br>
+            Miscellaneous
+        </h2>
+        <section class="akn-section" id="sec_34" data-eid="sec_34">
+            <h3>34. Offences and penalties<h3>
+            <section class="akn-subsection" id="sec_34__subsec_1" data-eid="sec_34__subsec_1">
+                <span class="akn-num">(1)</span>
+                <span class="akn-content">
+                    <span class="akn-blockList" id="sec_34__subsec_1__list_1" data-eid="sec_34__subsec_1__list_1">
+                        <span class="akn-listIntroduction">
+                            Any 
+                            <span class="akn-term term-link" data-refersto="#term-person" id="sec_34__subsec_1__list_1__term_1" data-eid="sec_34__subsec_1__list_1__term_1" data-original-title="" title="">
+                                person
+                            </span>
+                             who –
+                        </span>
+                        <span class="akn-item" id="sec_34__subsec_1__list_1__item_a" data-eid="sec_34__subsec_1__list_1__item_a">
+                            <span class="akn-num">(a)</span>
+                            <span class="akn-p">contravenes or fails to comply with any provision of this By-law or with any order or notice lawfully issued thereunder commits an offence; and</span>
+                        </span>
+                        <span class="akn-item" id="sec_34__subsec_1__list_1__item_b" data-eid="sec_34__subsec_1__list_1__item_b">
+                            <span class="akn-num">(b)</span>
+                            <span class="akn-p">continues to commit an offence after notice has been served on him or her to cease committing such offence or after he has been convicted of such offence shall be guilty of a continuing offence.</span>
+                        </span>
+                    </span>
+                </span>
+            </section>
+            <section class="akn-subsection" id="sec_34__subsec_2" data-eid="sec_34__subsec_2">
+                <span class="akn-num">(2)</span>
+                <span class="akn-content">
+                    <span class="akn-p">
+                        Any
+                        <span class="akn-term term-link" data-refersto="#term-person" id="sec_34__subsec_2__term_1" data-eid="sec_34__subsec_2__term_1" data-original-title="" title=""> person </span>
+                        convicted of an offence under this By-law shall be liable to a fine or imprisonment for a period not exceeding two years;
+                    </span>
+                </span>
+            </section>
+            <section class="akn-subsection" id="sec_34__subsec_3" data-eid="sec_34__subsec_3">
+                <span class="akn-num">(3)</span>
+                <span class="akn-content">
+                    <span class="akn-p">in the case of a continuing offence an additional fine or imprisonment for a period not exceeding ten days for each day on which such offence continued may be imposed or both a fine and imprisonment.</span>
+                </span>
+            </section>
+            <section class="akn-subsection" id="sec_34__subsec_4" data-eid="sec_34__subsec_4">
+                <span class="akn-num">(4)</span>
+                <span class="akn-content">
+                    <span class="akn-p">
+                        Any court convicting any 
+                        <span class="akn-term term-link" data-refersto="#term-person" id="sec_34__subsec_4__term_1" data-eid="sec_34__subsec_4__term_1" data-original-title="" title="">person </span>
+                        of keeping any 
+                        <span class="akn-term term-link" data-refersto="#term-animal" id="sec_34__subsec_4__term_2" data-eid="sec_34__subsec_4__term_2" data-original-title="" title="">animal</span>
+                         which is not kept under control in accordance with the provisions of this By-law may, in addition to the penalty referred to in subsection (2), order the destruction of the 
+                        <span class="akn-term term-link" data-refersto="#term-animal" id="sec_34__subsec_4__term_3" data-eid="sec_34__subsec_4__term_3" data-original-title="" title="">animal</span>
+                         concerned, and thereupon an 
+                        <span class="akn-term term-link" data-refersto="#term-authorised_official" id="sec_34__subsec_4__term_4" data-eid="sec_34__subsec_4__term_4" data-original-title="" title="">authorised official</span>
+                         may destroy such 
+                        <span class="akn-term term-link" data-refersto="#term-animal" id="sec_34__subsec_4__term_5" data-eid="sec_34__subsec_4__term_5" data-original-title="" title="">animal</span>.
+                    </span>
+                </span>
+            </section>
+        </section>
+        <section class="akn-section" id="sec_35" data-eid="sec_35">
+            <h3>35. Application to the state and council</h3>
+            <span class="akn-hcontainer" id="sec_35__hcontainer_1" data-eid="sec_35__hcontainer_1" data-name="hcontainer">
+                <span class="akn-content">
+                    <span class="akn-p">
+                        These By-laws bind the State and 
+                        <span class="akn-term term-link" data-refersto="#term-Council" id="sec_35__hcontainer_1__term_1" data-eid="sec_35__hcontainer_1__term_1" data-original-title="" title="">Council</span>.
+                    </span>
+                </span>
+            </span>
+        </section>
+        <section class="akn-section" id="sec_36" data-eid="sec_36">
+            <h3>36. Repeal</h3>
+            <span class="akn-hcontainer" id="sec_36__hcontainer_1" data-eid="sec_36__hcontainer_1" data-name="hcontainer">
+                <span class="akn-content">
+                    <span class="akn-blockList" id="sec_36__hcontainer_1__list_1" data-eid="sec_36__hcontainer_1__list_1">
+                        <span class="akn-listIntroduction">The following By-laws are hereby repealed:</span>
+                        <span class="akn-item" id="sec_36__hcontainer_1__list_1__item_a" data-eid="sec_36__hcontainer_1__list_1__item_a">
+                            <span class="akn-num">(a)</span>
+                            <span class="akn-p">Kuilsrivier Municipality By-law relating to the keeping of dogs as published in Provincial Notice No. 1027 of 1977;</span>
+                        </span>
+                        <span class="akn-item" id="sec_36__hcontainer_1__list_1__item_b" data-eid="sec_36__hcontainer_1__list_1__item_b">
+                            <span class="akn-num">(b)</span>
+                            <span class="akn-p">Milnerton Municipality By-law relating to the control of dogs as published in Provincial Notice No. 481 of 1987;</span>
+                        </span>
+                        <span class="akn-item" id="sec_36__hcontainer_1__list_1__item_c" data-eid="sec_36__hcontainer_1__list_1__item_c">
+                            <span class="akn-num">(c)</span>
+                            <span class="akn-p">Brackenfell Municipality By-law relating to the keeping of dogs as published in Provincial Notice No. 190 of 1979 and Amendment of By-law relating to the keeping of dogs as published in Provincial Notice No. 510 of 1990;</span>
+                        </span>
+                        <span class="akn-item" id="sec_36__hcontainer_1__list_1__item_d" data-eid="sec_36__hcontainer_1__list_1__item_d">
+                            <span class="akn-num">(d)</span>
+                            <span class="akn-p">Kraaifontein Municipality By-law for the Control of dogs as published in Provincial Notice No. 669 of 1979 and Amendment to the By-law for the control of dogs as published in Provincial Notice No. 526 of 1982;</span>
+                        </span>
+                        <span class="akn-item" id="sec_36__hcontainer_1__list_1__item_e" data-eid="sec_36__hcontainer_1__list_1__item_e">
+                            <span class="akn-num">(e)</span>
+                            <span class="akn-p">
+                                Divisional 
+                                <span class="akn-term term-link" data-refersto="#term-Council" id="sec_36__hcontainer_1__list_1__item_e__term_1" data-eid="sec_36__hcontainer_1__list_1__item_e__term_1" data-original-title="" title="">Council</span>
+                                 of Stellenbosch By-law relating to the keeping of dogs as published in Provincial Notice No. 692 of 1979;
+                            </span>
+                        </span>
+                        <span class="akn-item" id="sec_36__hcontainer_1__list_1__item_f" data-eid="sec_36__hcontainer_1__list_1__item_f">
+                            <span class="akn-num">(f)</span>
+                            <span class="akn-p">Fish Hoek Municipality dog controlled By-law as published in Provincial Notice No. 675 of 1979;</span>
+                        </span>
+                        <span class="akn-item" id="sec_36__hcontainer_1__list_1__item_g" data-eid="sec_36__hcontainer_1__list_1__item_g">
+                            <span class="akn-num">(g)</span>
+                            <span class="akn-p">Simonstown Municipality Regulations relating to the keeping of dogs as published in Provincial Notice No. 549 of 1963;</span>
+                        </span>
+                        <span class="akn-item" id="sec_36__hcontainer_1__list_1__item_h" data-eid="sec_36__hcontainer_1__list_1__item_h">
+                            <span class="akn-num">(h)</span><span class="akn-p">City of Tygerberg By-law relating to the control of dogs as published in Provincial Notice No. 567 of 2000;</span>
+                        </span>
+                        <span class="akn-item" id="sec_36__hcontainer_1__list_1__item_i" data-eid="sec_36__hcontainer_1__list_1__item_i">
+                            <span class="akn-num">(i)</span>
+                            <span class="akn-p">Cape Town Municipality By-law for the control of dogs as published in Provincial Notice No. 658 of 1981;</span>
+                        </span>
+                        <span class="akn-item" id="sec_36__hcontainer_1__list_1__item_j" data-eid="sec_36__hcontainer_1__list_1__item_j">
+                            <span class="akn-num">(j)</span>
+                            <span class="akn-p">
+                                Cape Divisional 
+                                <span class="akn-term term-link" data-refersto="#term-Council" id="sec_36__hcontainer_1__list_1__item_j__term_1" data-eid="sec_36__hcontainer_1__list_1__item_j__term_1" data-original-title="" title="">Council</span>
+                                 By-law relating to the control of dogs as published in Provincial Notice No. 781 of 1985;
+                            </span>
+                        </span>
+                        <span class="akn-item" id="sec_36__hcontainer_1__list_1__item_k" data-eid="sec_36__hcontainer_1__list_1__item_k">
+                            <span class="akn-num">(k)</span>
+                            <span class="akn-p">Part 2 of the Environmental Health By-law of the City of Cape Town as published in the Provincial Gazette Extraordinary dated 30 June 2003.</span>
+                        </span>
+                    </span>
+                </span>
+            </span>
+        </section>
+        <section class="akn-section" id="sec_37" data-eid="sec_37">
+            <h3>37. Transitional provisions</h3>
+            <section class="akn-subsection" id="sec_37__subsec_1" data-eid="sec_37__subsec_1">
+                <span class="akn-num">(1)</span>
+                <span class="akn-content">
+                    <span class="akn-p">A nine month transitional period for the registration of dogs in terms of section 3 (1) and section 3 (2) of this By-law will apply from the date of promulgation of this By-law by publication in the Gazette.</span>
+                </span>
+            </section>
+            <section class="akn-subsection" id="sec_37__subsec_2" data-eid="sec_37__subsec_2">
+                <span class="akn-num">(2)</span>
+                <span class="akn-content">
+                    <span class="akn-p">
+                        Applications in terms of section 4(1) of this By-law to keep a greater number of dogs on 
+                        <span class="akn-term term-link" data-refersto="#term-premises" id="sec_37__subsec_2__term_1" data-eid="sec_37__subsec_2__term_1" data-original-title="" title="">premises</span>
+                         than the number permitted in terms of section 2(2), will in the first six months after promulgation of this By-law, be granted up to a maximum of six dogs on any 
+                        <span class="akn-term term-link" data-refersto="#term-premises" id="sec_37__subsec_2__term_2" data-eid="sec_37__subsec_2__term_2" data-original-title="" title="">premises</span>
+                        , subject to the 
+                        <span class="akn-term term-link" data-refersto="#term-owner" id="sec_37__subsec_2__term_3" data-eid="sec_37__subsec_2__term_3" data-original-title="" title="">owner</span>
+                         not replacing any 
+                        <span class="akn-term term-link" data-refersto="#term-dog" id="sec_37__subsec_2__term_4" data-eid="sec_37__subsec_2__term_4" data-original-title="" title="">dog</span>
+                         that dies or is disposed of if it would result in a contravention of section 2(2).
+                    </span>
+                </span>
+            </section>
+            <section class="akn-subsection" id="sec_37__subsec_3" data-eid="sec_37__subsec_3">
+                <span class="akn-num">(3)</span>
+                <span class="akn-content">
+                    <span class="akn-p">
+                        Applications to keep a greater number of cats on 
+                        <span class="akn-term term-link" data-refersto="#term-premises" id="sec_37__subsec_3__term_1" data-eid="sec_37__subsec_3__term_1" data-original-title="" title="">premises</span>
+                         than the number permitted in terms of section 14(2), will in the first six months after promulgation of this By-law, be granted up to a maximum of six cats on any 
+                        <span class="akn-term term-link" data-refersto="#term-premises" id="sec_37__subsec_3__term_2" data-eid="sec_37__subsec_3__term_2" data-original-title="" title="">premises</span>
+                        , subject to the 
+                        <span class="akn-term term-link" data-refersto="#term-owner" id="sec_37__subsec_3__term_3" data-eid="sec_37__subsec_3__term_3" data-original-title="" title="">owner</span>
+                         not replacing any 
+                        <span class="akn-term term-link" data-refersto="#term-cat" id="sec_37__subsec_3__term_4" data-eid="sec_37__subsec_3__term_4" data-original-title="" title="">cat</span>
+                         that dies or is disposed of if it would result in a contravention of section 14(2).
+                    </span>
+                </span>
+            </section>
+            <section class="akn-subsection" id="sec_37__subsec_4" data-eid="sec_37__subsec_4">
+                <span class="akn-num">(4)</span>
+                <span class="akn-content">
+                    <span class="akn-p">Section 6(i) and section 14(6) of this By-law takes effect after a period of six months has elapsed since the date of promulgation of this By-law by publication in the Gazette.</span>
+                </span>
+            </section>
+            <section class="akn-subsection" id="sec_37__subsec_5" data-eid="sec_37__subsec_5">
+                <span class="akn-num">(5)</span>
+                <span class="akn-content">
+                    <span class="akn-p">Section 17(7) of this By-law takes effect after a period of six months has elapsed since the date of promulgation of this By-law by publication in the Gazette.</span>
+                </span>
+            </section>
+            <section class="akn-subsection" id="sec_37__subsec_6" data-eid="sec_37__subsec_6">
+                <span class="akn-num">(6)</span>
+                <span class="akn-content">
+                    <span class="akn-p">Section 12 (1) and section 15 (1) of this By-law takes effect after a period of six months has elapsed since the date of promulgation of this By-law by publication in the Government Gazette.</span>
+                </span>
+            </section>
+        </section>
+        <section class="akn-section" id="sec_38" data-eid="sec_38">
+            <h3>38. Short title</h3>
+            <span class="akn-hcontainer" id="sec_38__hcontainer_1" data-eid="sec_38__hcontainer_1" data-name="hcontainer">
+                <span class="akn-content">
+                    <span class="akn-p">This law is called the City of Cape Town: Animal By-law, 2010.</span>
+                </span>
+            </span>
+        </section>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'Chapter7'
+}
+</script>
